ffbeaa2664c0a8a6699127efe64e8e5813707f82
Owais Raza <owaisrazaraza780@gmail.com>
2025-06-11 09:02:56 +0500

    js assign

--- app.js
+++ app.js
@@ -1,65 +1,72 @@
+let btn = document.getElementById('btn')
+let btn2 = document.getElementById('btn2')
 
-          let btn = document.getElementById('btn')
-          let btn2 = document.getElementById('btn2')
-
-          let show = document.getElementById('show')
-          
-          
-          
-          
+let show = document.getElementById('show')
 
 
-          function add(){
-
-          
-            let form = document.getElementById('form');
-            form.style.display="block";
-            btn.style.display="none";
-           
-            show.classList.remove('form')
-          // show.innerHTML= '';
-             
-
-          }
-
-          
-function creat()   { 
-        form.style.display="none";
-            btn.style.display="block";
 
 
-          
-           let name = document.getElementById('name').value
-          let taskNo = document.getElementById('num').value     
-          let task = document.getElementById('task').value;
-            // Naya div banakar style de rahe hain
+
+
+function add() {
+
+
+    let form = document.getElementById('form');
+    form.style.display = "block";
+    btn.style.display = "none";
+
+    document.getElementById('body')
+    body.style.backgroundColor = "#8c8989";
+
+    show.classList.remove('form')
+    // show.innerHTML= '';
+
+
+}
+
+
+function creat() {
+    form.style.display = "none";
+    btn.style.display = "block";
+
+
+
+    let name = document.getElementById('name').value
+    let title = document.getElementById('title').value
+    let task = document.getElementById('task').value;
+    
+    
+    // Naya div banakar style dya
     let newBox = document.createElement('div');
     newBox.style.border = "1px solid #000";
     newBox.style.padding = "10px";
     newBox.style.marginTop = "10px";
-          
+    newBox.style.borderRadius="10px"
+    body.style.backgroundColor = "white";
 
-    // Content set karte hain
+
+
+    // Content set hain
     newBox.innerHTML += `
-      <h3>Name: ${name}</h3>
-      <h3>Task No: ${taskNo}</h3>
-      <h3>Task: ${task}</h3>
-      <button onclick="clearTask(this)">Clear</button>
+    <h3>Name: ${name}</h3>
+    <h3>Titile: ${title}</h3>
+    <h3>Task: ${task}</h3>
+    <button onclick="remove(this)">Clear</button>
     `;
-show.classList.add('form'); 
+    show.classList.add('form');
 
     // Show container mein add kar dena
-show.appendChild(newBox);
+    show.appendChild(newBox);
 
-    // Form ke input clear 
-  document.getElementById('name').value = '';
-  document.getElementById('num').value = '';
-  document.getElementById('task').value = '';
-                           
+    // Form ke input clear
+    document.getElementById('name').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('task').value = '';
+
 }
 
-function clearTask(btn) {
+function remove(btn) {
     let div = btn.parentElement;
     document.getElementById('show').removeChild(div);
-    
-  }
+
+}
